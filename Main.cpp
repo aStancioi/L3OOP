57584c2f995fcfd18a60c73c6ed4f0c6a7980a3a
SMara99 <61970933+SMara99@users.noreply.github.com>
2020-04-13 22:25:06 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -11,8 +11,16 @@ int main()
 	Tree tree(10);
 	tree.insert(5);
 	tree.insert(12);
+	
 	assert(tree.countNodes() == 3);
 	assert(tree.countEdges() == 2);
 	assert(tree.height() == 2);
+	
+	cout << "\nBinarbaum inorder" << tree.inorder();
+	cout << "\nBinarbaum postorder" << tree.postorder();
+	cout << "\nBinarbaum preorder" << tree.preorder();
+	
+	tree.remove(5)
+	
 	return 0;
 }
