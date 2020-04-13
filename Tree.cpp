c3ef352af8d44cab7f0cbaf841e351418df16ae3
SMara99 <61970933+SMara99@users.noreply.github.com>
2020-04-13 22:17:41 +0300

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -79,3 +79,107 @@ unsigned int Tree::height()
 {
     return height(root);
 }
+
+//Binarbaum inorder
+string Tree::inorder(Node *node) {
+	//string result;
+	if (node == nullptr)
+	{
+		return "";
+	}
+	return inorder(node->left) + to_string(node->value) + " " + inorder(node->right);
+	
+
+}
+
+string Tree::inorder() {
+    return inorder(root);
+}
+
+//Binarbaum preorder
+string Tree::preorder(Node *node)
+{
+	string result;
+	if (node == nullptr)
+	{
+		return "";
+	}
+	return  to_string(node->value) + " " + preorder(node->left) + preorder(node->right);
+}
+
+string Tree::preorder() {
+   return preorder(root);
+	
+}
+
+//Binarbaum postorder
+string Tree::postorder(Node *node) {
+	
+		string result;
+		if (node == nullptr)
+		{
+			return "";
+		}
+		return preorder(node->left) + preorder(node->right)+ to_string(node->value) + " ";
+
+}
+
+string Tree::postorder() {
+    return postorder(root);
+}
+
+
+//entfernen
+Node *Tree::remove(int value, Node *node) {
+    if(node == nullptr)
+        return nullptr;
+
+    if (value < node->value) {
+            node->left = remove(value, node->left);
+    }
+    else if (value > node->value) {
+            node->right = remove(value, node->right);
+    }
+    else {
+
+        if(node->left == nullptr && node->right == nullptr)
+        {
+            delete node;
+            node = nullptr;
+        }
+        else if(node->left == nullptr)
+        {
+            Node *temp;
+            temp = node;
+            node = node->right;
+            delete temp;
+        }
+        else if(node->right == nullptr)
+        {
+            Node *temp;
+            temp = node;
+            node = node->left;
+            delete temp;
+        }
+        else
+        {
+            Node *min = getMinNode(node->right);
+            node->value = min->value;
+            node->right = remove(min->value, node->right);
+        }
+    }
+   return node;
+}
+
+void Tree::remove(int v) {
+    root = remove(v, root);
+}
+
+Node *Tree::getMinNode(Node *node) {
+    if(node == nullptr)
+        return nullptr;
+    Node *current = node;
+    while (current->left != nullptr)
+        current = current->left;
+    return current;
+}
